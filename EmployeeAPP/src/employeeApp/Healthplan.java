40673303d732171cf0d91d216c915d9606d3f80a
Samsoucy <newf930@outlook.com>
2020-01-12 11:59:04 -0500

    addded maxId to class to keep track of and asign employee ids

--- EmployeeAPP/src/employeeApp/Healthplan.java
+++ EmployeeAPP/src/employeeApp/Healthplan.java
@@ -2,11 +2,14 @@ package employeeApp;
 
 public class Healthplan
 {
+    private static int maxId = 0;
     private int id;
     private String name;
 
     public Healthplan(String name)
     {
+        maxId++;
+        id = maxId;
         this.name = name;
     }
 
