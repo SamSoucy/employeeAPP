947f209ca491a87df3853b4e7e274dc87663acff
SamSoucy <43486361+SamSoucy@users.noreply.github.com>
2020-01-12 10:25:54 -0500

    Add files via upload

---
+++ EmployeeAPP/src/employeeApp/Employee.java
@@ -0,0 +1,18 @@
+package employeeApp;
+
+public class Employee
+{
+    //fields
+    private int id;
+    private String fname;
+    private String lname;
+    private double salary;
+    private boolean has401k;
+    private int companyId;
+    private int healthPlanId;
+
+
+    //methods- geters and setters
+
+    //methods
+}
---
+++ EmployeeAPP/src/employeeApp/Main.java
@@ -0,0 +1,12 @@
+package employeeApp;
+
+public class Main
+{
+    //field - information
+    //methods- do behaviors
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
