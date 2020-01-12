d5905cec9ef60e5cc694304525d6b7f708504264
Samsoucy <newf930@outlook.com>
2020-01-12 12:08:01 -0500

    added new file company.java. added class company. added methods for id, name and debt and maxId. added a final static method so the company matches the employee 401k contribution

---
+++ EmployeeAPP/src/employeeApp/Company.java
@@ -0,0 +1,19 @@
+package employeeApp;
+
+public class Company
+{
+    private static int maxId = 0;
+    public final static double match401k = 0.05;
+
+    public int id;
+    public String name;
+    public int debt;
+
+    public Company(String name, int debt)
+    {
+        maxId++;
+        this.id = maxId;
+        this.name = name;
+        this.debt = debt;
+    }
+}
