01c0ef3a78e728241c3888a5c0fa927ec22a1dd9
Samsoucy <newf930@outlook.com>
2020-01-13 20:02:36 -0500

    created two health plans, two companies, and two employees

--- EmployeeAPP/src/employeeApp/Main.java
+++ EmployeeAPP/src/employeeApp/Main.java
@@ -7,6 +7,15 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println("Hello");
+        // System.out.println("Hello");
+
+        Healthplan h1 = new Healthplan("My Health 1");
+        Healthplan h2 = new Healthplan("My Health 2");
+
+        Company c1 = new Company("Company A", 100);
+        Company c2 = new Company("Company B", 100);
+
+        Employee emp1 = new Employee("Steve", "Green", 45000, true, c1.id, h1.getId());
+        Employee emp2 = new Employee("May", "Ford", 80000, true, c1.id, h2.getId());
     }
 }
